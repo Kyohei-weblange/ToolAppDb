716e62db37fe3a8ae0392bf21f8f3338fc041e1e
Kyohei-weblange <kyo.webridge358@gmail.com>
2026-08-04 00:13:42 +0900

    feat: SQLiteの導入とToolsテーブルの自動作成処理を追加

--- Program.vb
+++ Program.vb
@@ -1,7 +1,29 @@
 Imports System
+Imports Microsoft.AspNetCore.Builder
+Imports Microsoft.Data.Sqlite
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Dim builder = WebApplication.CreateBuilder(args)
+        Dim app = builder.Build()
+        Dim connectionString As String = "Data Source=tools.db"
+
+        app.useDefaultFiles()
+        app.useStaticFiles()
+
+        Using connection As New SqliteConnection(connectionString)
+            Connection.Open()
+            Dim command = connection.CreateCommand()
+            command.CommandText = "
+            CREATE TABLE IF NOT EXISTS Tools (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Name TEXT NOT NULL,
+                Storage TEXT NOT NULL,
+                IsAvailable INTEGER NOT NULL
+                )
+            "
+            command.ExecuteNonQuery()
+        End Using
+        app.Run()
     End Sub
 End Module
